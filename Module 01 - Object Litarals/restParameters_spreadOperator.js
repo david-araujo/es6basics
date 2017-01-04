5ea704220c397f802814fd70d1c281452ec0fe0b
David Araujo <davsra@br.ibm.com>
2017-01-04 12:31:20 -0200

    Rest Parameter and Spread Operator examples.

--- Module 01 - Object Litarals/restParameters_spreadOperator.js
+++ Module 01 - Object Litarals/restParameters_spreadOperator.js
@@ -1,31 +1,46 @@
 /*
-    Spread operator ... is a new functionallity that is similar to arguments but with some caveats.
+    Rest Parameter and Sread Operator ...
 */
 
-// Using arguments returns a confusing and numered object of parameters sent to the function spread.
-function spread (){
+// Using arguments returns a confusing and numered object of parameters sent to the function cast.
+function cast (){
     return arguments;
 }
 
-var args = spread(1, 'teste', {name: 'David'}, [1,2,3,4]);
-
-console.log(args);
+var args = cast(1, 'teste', {name: 'David'}, [1,2,3,4]);
+console.log('arguments on ES5 is soooo confuse... \\o?\n', args);
 console.log('******');
 
-// Using the new spread operator you can do AMAZING THINGS...
-function spread1 (...list){
+function cast1 (...list){
     return list;
 }
 
-var args = spread1 (1, 'teste', {name: 'David'}, [1,2,3,4]);
+var args = cast1 (1, 'teste', {name: 'David'}, [1,2,3,4]);
 
 console.log('ES6 returns an array: ', args);
 console.log('******');
 
 // Let's supose that you want recive 2 parameters and keep the rest in an array
-function spread2 (p1, p2, ...others){
-    console.log('The p1 = ' + p1 + ' p2 = ' + p2 + ' and others is an array... ', others );
+function cast2 (p1, p2, ...rest){
+    console.log('The p1 = ' + p1 + ' p2 = ' + p2 + ' and rest is an array... ', rest );
 }
 
-var args = spread2 (1, 'teste', {name: 'David'}, [1,2,3,4]);
+var args = cast2 (1, 'teste', {name: 'David'}, [1,2,3,4]);
 console.log('******');
+
+// Spread is an useful way of combining multiple arrays.
+var arr1 = [2,3,4];
+var arr2 = ['a', 'b', 'c'];
+var arrS = [1, ...arr2, 5, ...arr1];
+
+console.log("SPREAD OPERATOR is an incredible way to join arrays: \n", arr1, arr2, arrS);
+console.log('******');
+
+// Besides spreading onto arrays, you can also spread items onto function calls.
+function multiply (a, b, ...rest){
+    console.log('LOOK AT THE CODE to understand the magic: ',rest);
+    return a * b;
+}
+
+var teste = multiply(...[2,3,4,5,'a','b','c']);
+console.log('It\'s AWESOME!!! Spread an array into variables and passing to the function... \\o/ ',teste);
